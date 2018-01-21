0e2aa01657e18ea291a5eac4eee1717c22b043a6
Kyle <kyle.b.goonrey@hotmail.com>
2018-01-21 11:13:27 +1000

    Adding more functionality to the webportal

---
+++ Trunk/WebPortal/App_Code/EmailClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace WebPortal.App_Code
+{
+    public class EmailClass
+    {
+        public string SendEmail(string test)
+        {
+            MailMessage mail = new MailMessage("FarmMateNotifications@gmail.com", "toemailaddress@hotmail.com");
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("FarmMateNotifications@gmail.com", "******");
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            mail.Subject = "this is a test email.";
+            mail.Body = "this is my test email body";
+            client.Send(mail);
+
+            return "Success";
+        }
+    }
+}
